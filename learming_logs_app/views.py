148c2124ec17ee74fbcb7adb5d14b227cca10f9e
mnoskovv <nikita01noskov@gmail.com>
2020-03-23 14:47:57 +0200

    develop

---
+++ learming_logs_app/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def index(request):
+    """Домашняя страница приложения Learning log"""
+    return render(request, 'learming_logs_app/index.html')
+
+@login_required
+def topics(request):
+    """displays a list of themes related to user"""
+    topics = Topic.objects.filter(owner = request.user).order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learming_logs_app/topics.html', context)
+
+@login_required
+def topic(request, topic_id):
+    """displays all records about single topic"""
+    try:
+        topic = Topic.objects.get(id=topic_id)
+    except:
+        raise Http404
+    if topic.owner != request.user:
+        raise Http404
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learming_logs_app/topic.html', context)
+
+@login_required
+def new_topic(request):
+    """Определяет новую тему"""
+    if request.method != 'POST':
+        # Данные не отправлялисть, создается пустая форма
+        form = TopicForm()
+    else:
+        # Было отправлено что-то через POST, обрабатываем данные
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            new_topic = form.save(commit = False)
+            new_topic.owner = request.user
+            new_topic.save()
+            return HttpResponseRedirect(reverse('learming_logs_app:topics'))
+
+    context = {'form': form}
+    return render(request, 'learming_logs_app/new_topic.html', context)
+
+@login_required
+def new_entry(request, topic_id):
+    """Adds new info by chosen theme"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learming_logs_app:topic', args = [topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learming_logs_app/new_entry.html', context)
+
+@login_required
+def edit_entry(request, entry_id):
+    """Allows to edit selected entry"""
+    entry = Entry.objects.get(id = entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+
+    if request.method != 'POST':
+        form = EntryForm(instance = entry)
+    else:
+        form = EntryForm(instance = entry, data = request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learming_logs_app:topic', args = [topic.id]))
+    
+    context = {'entry' : entry, 'topic' : topic, 'form' : form}
+    return render(request, 'learming_logs_app/edit_entry.html', context)
---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('learming_logs_app:index'))
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data = request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # Выполнение входа и перенаправление на домашнюю страницу.
+            authenticated_user = authenticate(username = new_user.username, password = request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learming_logs_app:index'))
+
+    context = {'form' : form}
+    return render(request, 'users/register.html', context)

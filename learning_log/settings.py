ca32e7a3cc6f4a056efc217fbea1ba044ba833f8
mnoskovv <nikita01noskov@gmail.com>
2020-03-24 13:19:31 +0200

    nu suka

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -59,7 +59,10 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'learming_logs_app/templates/'),
+            os.path.join(BASE_DIR,'users/templates/'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

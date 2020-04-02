f6ea0e4ffdb999ef716ffafcb02c46323a7ca923
mnoskovv <nikita01noskov@gmail.com>
2020-04-02 20:54:44 +0300

    rm static files

--- staticfiles/script.js
+++
@@ -1,8 +0,0 @@
-function confirmDelEntry(obj) {
-    let answer = confirm("Are you shure to delete this record from your log?");
-    
-    if (answer == false) {
-        obj.href = "#";
-    }
-    return false;
-}
